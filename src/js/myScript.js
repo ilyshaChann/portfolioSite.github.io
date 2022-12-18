b80e0b024fdae53063a34ddb2c5f30d390e4b166
ilyshaChann <109521499+ilyshaChann@users.noreply.github.com>
2022-12-18 17:15:47 +0300

    Доработка проекта

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -91,19 +91,113 @@ for (let key in objTerms){
 
 confirm("Конечная стоимость сайта: " + objSum + "руб.\n\nСроки для созданания сайта: " + objSumTerms + " дней." + "\n\nЕсли что-то не устраивает, вы всегда можете обратиться  ко мне по почте (ссылку на нее можно найти в 'шапке' сайта).")*/
 
-/*$(document).ready(function(){
+$(document).ready(function(){	
+	function calc(){
+	let sumCalc = parseInt($('#sel_1 option:selected').val()) + parseInt($('#sel_2 option:selected').val()) + parseInt($('#sel_3 option:selected').val());
+	let daysCalc = parseInt($('#sel_1 option:selected').attr('days')) + parseInt($('#sel_2 option:selected').attr('days')) + parseInt($('#sel_3 option:selected').attr('days'));
+	$('.terms h4').text(daysCalc);
+	$('.cost h4').text(sumCalc);
+}	
+$('select').on('change', function(){
+	calc();
+});
+calc();
+	
 $(window).scroll(() => {
 	let scrollDist = $(window).scrollTop();
-	$(.)
+	$(".section").each((i, el) => {
+		if($(el).offset().top - $("nav").outerHeight() <= scrollDist){
+			$("nav a").each((i, el) => {
+				if($(el).hasClass("li_menu")){
+					$(el).removeClass("li_menu");
+				}
+			});
+			$('nav li:eq('+ i +')').find('a').addClass('li_menu');
+		}
+	});
 });
-});*/	  
+	let options = {
+            threshold: [0.3]
+        };
+        let observer = new IntersectionObserver(onEntry, options);
+        let elements = $('.sect');
+
+        elements.each((i, el) => {
+            observer.observe(el);
+        });
+
+
+        function onEntry(entry) {
+            entry.forEach(change => {
+                if (change.isIntersecting) {
+                    change.target.classList.add('sect-show');
+                }
+            });
+        };
+	
+			let optionsImage = {
+				threshold: [0.2]
+			};
+			let observerImage = new IntersectionObserver(onEntryImage, optionsImage);
+			let elementsImage = $('.all_photo');
+
+			elementsImage.each((i, el) => {
+				observerImage.observe(el);
+			});
+
+
+			function onEntryImage(entry) {
+				entry.forEach(change => {
+					if (change.isIntersecting) {
+						change.target.src = change.target.dataset.src;					
+					};
+				});
+			};
+	
+			$(window).addEventListener('load', setTimeout(function () {
+			const elemModal = document.querySelector('#modal');
+			const modal = new bootstrap.Modal(elemModal);
+			modal.show();
+			}, 10000));
+			
+				let optionsStatistic = {
+					threshold: [0.3]
+				};
+				let observerStatistic = new IntersectionObserver(onEntryStatistic, optionsStatistic);
+				let elementsStatistic = $('.animate_numbers');
+
+				elementsStatistic.each((i, el) => {
+					observerStatistic.observe(el);
+				});
+
+
+				function onEntryStatistic(entry) {
+            		entry.forEach(change => {
+						if (change.isIntersecting) {
+							$('.animate_numbers').spincrement({
+							 thousandSeparator: "",
+							 duration: 3000
+                    });
+				}
+			});
+		};
+				$('.image-link').magnificPopup({
+					type: 'image'
+				});
+});	
+	
 
 $(window).on('load', function(){
     $(".loader").fadeOut().end().delay(500).fadeOut('slow');    
+	
 
 });
 
 $("a[href^='#']").click(function(){
 	let valHref = $(this).attr("href");
 	$("html, body").animate({scrollTop: $(valHref).offset().top - 55 + 'px'});
-})
+});
+
+
+
+
